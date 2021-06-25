9214de8bb8582ff8fcba8edd2750acf60becc8f0
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-06-25 21:07:31 +0530

    changed it to controlled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,23 +4,43 @@ class Login extends Component {
     constructor(props)
     {
         super(props);
-        this.emailInputRef=React.createRef();
-        this.passwordInputRef=React.createRef();
+        // this.emailInputRef=React.createRef();
+        // this.passwordInputRef=React.createRef();
+        this.state={
+          email:'',
+          password:''
+        }
     }
     handleFormSubmit=(e)=>{
         e.preventDefault();
-        console.log('this.emailInputRef',this.emailInputRef);
-        console.log('this.passwordInputRef',this.passwordInputRef)
+        console.log('this.email',this.email);
+        console.log('this.password',this.password);
+        // console.log('this.emailInputRef',this.emailInputRef);
+        // c
+        // console.log('this.passwordInputRef',this.passwordInputRef)
+    }
+    handleEmailChange=(e)=>{
+      // console.log(this.email)
+      // this.email=e.target.value
+      this.setState({
+        email:e.target.value
+      })
+    }
+    handlePasswordChange=(e)=>{
+      // this.password=e.target.value
+      this.setState({
+        email:e.target.value
+      })
     }
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required ref={this.emailInputRef}/>
+          <input type="email" placeholder="Email" required /*ref={this.emailInputRef}*/ onChange={this.handleEmailChange}  value={this.email}/>
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required ref={this.passwordInputRef}/>
+          <input type="password" placeholder="Password" required onChange={this.handlePasswordChange} value={this.password}/>
         </div>
         <div className="field">
           <button onClick={this.handleFormSubmit}>Log In</button>
