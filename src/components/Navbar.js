a5f52c761f9218b9e0e02a02c4754e0757a8f3ce
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-07-01 15:33:37 +0530

    added the search functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,92 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { logoutUser } from '../actions/auth';
+import { searchUsers } from '../actions/Search';
+import search from '../reducers/search';
+import { searchUsers } from '../actions/Search';
 class Navbar extends React.Component {
-  logout=()=>{
+  logout = () => {
     localStorage.removeItem('token');
     this.props.dispatch(logoutUser());
-  }
-  render() { const{auth}=this.props
+  };
+  handleSearch = (e) => {
+    const searchText = e.target.value;
+    this.props.dispatch(searchUsers(searchText));
+  };
+  render() {
+    const { auth, results } = this.props;
     return (
-        <nav className="nav">
-          <div className="left-div">
-          <Link to='/'>
+      <nav className="nav">
+        <div className="left-div">
+          <Link to="/">
             <img
               src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
               alt="logo"
-            /></Link>
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://image.flaticon.com/icons/svg/483/483356.svg"
-              alt="search-icon"
             />
-            <input placeholder="Search" />
-
+          </Link>
+        </div>
+        <div className="search-container">
+          <img
+            className="search-icon"
+            src="https://image.flaticon.com/icons/svg/483/483356.svg"
+            alt="search-icon"
+          />
+          <input placeholder="Search" onChange={this.handleSearch} />
+          {results.lenght > 0 && (
             <div className="search-results">
               <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
+                {results.map((user) => (
+                  <li key={user._id} className="search-results-row">
+                    <Link to={`/user/${user._id}`}>
+                      <img
+                        src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                        alt="user-dp"
+                      />
+                      <span>{user.name}</span>
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
-          </div>
-          <div className="right-nav">
-            {auth.isLoggedin&&<div className="user">
+          )}
+        </div>
+        <div className="right-nav">
+          {auth.isLoggedin && (
+            <div className="user">
               <img
                 src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
                 alt="user-dp"
                 id="user-dp"
               />
               <span>{auth.user.name}</span>
-            </div>}
-            <div className="nav-links">
-              <ul>
-                {auth.isLoggedin&&<li><Link to='/login'>
-                Log in</Link></li>}
-                {auth.isLoggedin&&<li onClick={this.logout}>Log out</li>}
-                {!auth.isLoggedin&&<li> <Link to='/signup'>Register</Link></li>}
-              
-              </ul>
             </div>
+          )}
+          <div className="nav-links">
+            <ul>
+              {auth.isLoggedin && (
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>
+              )}
+              {auth.isLoggedin && <li onClick={this.logout}>Log out</li>}
+              {!auth.isLoggedin && (
+                <li>
+                  {' '}
+                  <Link to="/signup">Register</Link>
+                </li>
+              )}
+            </ul>
           </div>
-        </nav>
+        </div>
+      </nav>
     );
   }
 }
-function mapStateToProps(state)
-{
-  return{
-    auth:state.auth
-  }
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+    results: state.search.results,
+  };
 }
-export default connect(mapStateToProps)(Navbar)
-
-
+export default connect(mapStateToProps)(Navbar);
